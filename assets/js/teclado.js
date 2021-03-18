b67f3eafa2fdfd6a9877037abeedb3a07b3c7e1c
mavallejos93 <mavallejos93@gmail.com>
2021-03-17 21:16:04 -0500

    Reto TecladoJs BIT

---
+++ assets/js/teclado.js
@@ -0,0 +1,174 @@
+//Global
+let tablero = document.getElementById("tablero");
+
+//Primero linea vertical Teclado
+let btnEsc = document.getElementById("btnEsc");
+let btnNum = document.getElementById("btnNum");
+let btnNum1 = document.getElementById("btnNum1");
+let btnNum2 = document.getElementById("btnNum2");
+let btnNum3 = document.getElementById("btnNum3");
+let btnNum4 = document.getElementById("btnNum4");
+let btnNum5 = document.getElementById("btnNum5");
+let btnNum6 = document.getElementById("btnNum6");
+let btnNum7 = document.getElementById("btnNum7");
+let btnNum8 = document.getElementById("btnNum8");
+let btnNum9 = document.getElementById("btnNum9");
+let btnNum0 = document.getElementById("btnNum0");
+let btnComilla = document.getElementById("btnComilla");
+let btnInterrA = document.getElementById("btnInterrA");
+let btnBksc = document.getElementById("btnBksc");
+
+//segunda linea vertical teclado
+let btnTab = document.getElementById("btnTab");
+let btnLetq = document.getElementById("btnLetq");
+let btnLetw = document.getElementById("btnLetw");
+let btnLete = document.getElementById("btnLete");
+let btnLetr = document.getElementById("btnLetr");
+let btnLett = document.getElementById("btnLett");
+let btnLety = document.getElementById("btnLety");
+let btnLetu = document.getElementById("btnLetu");
+let btnLeti = document.getElementById("btnLeti");
+let btnLeto = document.getElementById("btnLeto");
+let btnLetp = document.getElementById("btnLetp");
+let btnTilde = document.getElementById("btnTilde");
+let btnMas = document.getElementById("btnMas");
+
+//tercera linea vertical teclado
+let btnBloqMay = document.getElementById("btnBloqMay");
+let btnLeta = document.getElementById("btnLeta");
+let btnLets = document.getElementById("btnLets");
+let btnLetd = document.getElementById("btnLetd");
+let btnLetf = document.getElementById("btnLetf");
+let btnLetg = document.getElementById("btnLetg");
+let btnLeth = document.getElementById("btnLeth");
+let btnLetj = document.getElementById("btnLetj");
+let btnLetk = document.getElementById("btnLetk");
+let btnLetl = document.getElementById("btnLetl");
+let btnLetene = document.getElementById("btnLetene");
+let btnCorcheteA = document.getElementById("btnCorcheteA");
+let btnCorcheteC = document.getElementById("btnCorcheteC");
+let btnEnter = document.getElementById("btnEnter");
+
+//cuarta linea vertical teclado
+let btnShift = document.getElementById("btnShift");
+let btnMenorq = document.getElementById("btnMenorq");
+let btnLetz = document.getElementById("btnLetz");
+let btnLetx = document.getElementById("btnLetx");
+let btnLetc = document.getElementById("btnLetc");
+let btnLetv = document.getElementById("btnLetv");
+let btnLetb = document.getElementById("btnLetb");
+let btnLetn = document.getElementById("btnLetn");
+let btnLetm = document.getElementById("btnLetm");
+let btnComa = document.getElementById("btnComa");
+let btnPunto = document.getElementById("btnPunto");
+let btnGuion = document.getElementById("btnGuion");
+/* let btnShift = document.getElementById("btnShift"); */ /* repetido */
+
+//quinta linea vertical teclado
+let btnFn = document.getElementById("btnFn");
+let btnCtrl = document.getElementById("btnCtrl");
+let btnWin = document.getElementById("btnWin");
+let btnAlt = document.getElementById("btnAlt");
+let btnEspacio = document.getElementById("btnEspacio");
+let btnAltGr = document.getElementById("btnAltGr");
+/* let btnCtrl = document.getElementById("btnCtrl"); */  /* repetido */
+
+//funcones
+
+//linea 1
+const btnEscF = () => {};
+const btnNum1F = () => {};
+const btnNum2F = () => {};
+const btnNum3F = () => {};
+const btnNum4F = () => {};
+const btnNum5F = () => {};
+const btnNum6F = () => {};
+const btnNum7F = () => {};
+const btnNum8F = () => {};
+const btnNum9F = () => {};
+const btnNum0F = () => {};
+const btnComillaF = () => {};
+const btnInterrAF = () => {};
+const btnBkscF = () => {};
+
+//linea 2
+const btnTabF = () => {};
+const btnLetqF = () => {};
+const btnLetwF = () => {};
+const btnLeteF = () => {};
+const btnLetrF = () => {};
+const btnLettF = () => {};
+const btnLetyF = () => {};
+const btnLetuF = () => {};
+const btnLetiF = () => {};
+const btnLetoF = () => {};
+const btnLetpF = () => {};
+const btnTildeF = () => {};
+const btnMasF = () => {};
+
+//linea 3
+const btnBloqMayF = () => {};
+const btnLetaF = () => {};
+const btnLetsF = () => {};
+const btnLetdF = () => {};
+const btnLetfF = () => {};
+const btnLetgF = () => {};
+const btnLethF = () => {};
+const btnLetjF = () => {};
+const btnLetkF = () => {};
+const btnLetlF = () => {};
+const btnLetpeneF = () => {};
+const btnCorcheteAF = () => {};
+const btnCorcheteCF = () => {};
+const btnEnterF = () => {};
+
+//linea 4
+const btnShiftF = () => {};
+const btnMenorqF = () => {};
+const btnLetzF = () => {};
+const btnLetxF = () => {};
+const btnLetcF = () => {};
+const btnLetvF = () => {};
+const btnLetbF = () => {};
+const btnLetnF = () => {};
+const btnLetmF = () => {};
+const btnComaF = () => {};
+const btnPuntoF = () => {};
+const btnGuionF = () => {};
+
+//linea 5
+const btnFnF = () => {};
+const btnCtrlF = () => {};
+const btnWinF = () => {};
+const btnAltF = () => {};
+const btnEspacioF = () => {};
+const btnAltGrF = () => {};
+
+//Onclick
+
+//linea 1
+btnEsc.onclick = function () {btnEscF();};
+btnNum1.onclick = function () {btnNum1F();};
+btnNum2.onclick = function () {btnNum2F();};
+btnNum3.onclick = function () {btnNum3F();};
+btnNum4.onclick = function () {btnNum4F();};
+btnNum5.onclick = function () {btnNum5F();};
+btnNum6.onclick = function () {btnNum6F();};
+btnNum7.onclick = function () {btnNum7F();};
+btnNum8.onclick = function () {btnNum8F();};
+btnNum9.onclick = function () {btnNum9F();};
+btnNum0.onclick = function () {btnNum0F();};
+btnComilla.onclick = function () {btnComillaF();};
+btnInterrA.onclick = function () {btnInterrAF();};
+btnBksc.onclick = function () {btnBkscF();};
+
+
+
+
+
+
+
+
+
+
+
